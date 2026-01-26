f7acabf4d1adfedb82ecdbce1e550404461b91f7
raphaelmelo07 <raphaelmelogarcia@gmail.com>
2026-01-25 21:32:52 -0300

    feat(domain): add date range value object

---
+++ DecorRental.Domain/ValueObjects/DateRange.cs
@@ -0,0 +1,24 @@
+﻿using DecorRental.Domain.Exceptions;
+
+namespace DecorRental.Domain.ValueObjects
+{
+    public sealed class DateRange
+    {
+        public DateOnly Start { get; set; }
+        public DateOnly End { get; set; }
+
+        public DateRange(DateOnly start, DateOnly end)
+        {
+            if (end < start)
+                throw new DomainException("End date must be after start date.");
+
+            Start = start;
+            End = end;
+        }
+
+        public bool Overlaps(DateRange other)
+        {
+            return Start <= other.End && End >= other.Start;
+        }
+    }
+}
