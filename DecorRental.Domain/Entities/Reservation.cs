c52f6e7dd423f317d860aa917e4e6f55fcef2ac4
raphaelmelo07 <raphaelmelogarcia@gmail.com>
2026-01-28 21:32:52 -0300

    feat(domain): add reservation entity

--- DecorRental.Domain/Entities/Reservation.cs
+++ DecorRental.Domain/Entities/Reservation.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DecorRental.Domain.Enums;
+using DecorRental.Domain.ValueObjects;
 
 namespace DecorRental.Domain.Entities
 {
-    internal class Reservation
+    public class Reservation
     {
+        public Guid Id { get; private set; }
+        public Guid KitId { get; private set; }
+        public DateRange Period { get; private set; }
+        public ReservationStatus Status { get; private set; }
+
+        public Reservation(Guid kitId, DateRange period)
+        {
+            Id = Guid.NewGuid();
+            KitId = kitId;
+            Period = period;
+            Status = ReservationStatus.Active;
+        }
+
+        public void Cancel()
+        {
+            Status = ReservationStatus.Cancelled;
+        }
     }
 }
