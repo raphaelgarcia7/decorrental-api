5b3d50a0daf0db74bcc9887a5b15b79861a2db70
raphaelmelo07 <raphaelmelogarcia@gmail.com>
2026-02-03 22:52:41 -0300

    feat: add kit reservation rules

---
+++ DecorRental.Domain/Entities/Kit.cs
@@ -0,0 +1,33 @@
+﻿using DecorRental.Domain.Enums;
+using DecorRental.Domain.Exceptions;
+using DecorRental.Domain.ValueObjects;
+
+namespace DecorRental.Domain.Entities;
+
+public class Kit
+{
+    private readonly List<Reservation> _reservations = new();
+
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+
+    public IReadOnlyCollection<Reservation> Reservations => _reservations;
+
+    public Kit(string name)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+    }
+
+    public void Reserve(DateRange period)
+    {
+        var hasConflict = _reservations.Any(r =>
+            r.Status == ReservationStatus.Active &&
+            r.Period.Overlaps(period));
+
+        if (hasConflict)
+            throw new DomainException("Kit is already reserved for this period.");
+
+        _reservations.Add(new Reservation(Id, period));
+    }
+}
---
+++ DecorRentals.Tests/Entities/KitTests.cs
@@ -0,0 +1,24 @@
+﻿using DecorRental.Domain.Entities;
+using DecorRental.Domain.Exceptions;
+using DecorRental.Domain.ValueObjects;
+using Xunit;
+
+namespace DecorRental.Tests.Entities;
+
+public class KitTests
+{
+    [Fact]
+    public void Should_not_allow_overlapping_reservations()
+    {
+        var kit = new Kit("Basic Kit");
+
+        kit.Reserve(new DateRange(
+            new DateOnly(2026, 1, 10),
+            new DateOnly(2026, 1, 12)));
+
+        Assert.Throws<DomainException>(() =>
+            kit.Reserve(new DateRange(
+                new DateOnly(2026, 1, 11),
+                new DateOnly(2026, 1, 13))));
+    }
+}
