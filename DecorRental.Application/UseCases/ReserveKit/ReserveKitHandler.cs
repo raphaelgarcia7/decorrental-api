5e4086c3c821c82b2f178aba689ae9b65b4aa4a7
raphaelgarcia7 <raphaelmelogarcia@gmail.com>
2026-02-05 13:37:27 -0300

    refactor(application): use kit repository in reserve use case

--- DecorRental.Application/UseCases/ReserveKit/ReserveKitHandler.cs
+++ DecorRental.Application/UseCases/ReserveKit/ReserveKitHandler.cs
@@ -1,16 +1,30 @@
 ﻿using DecorRental.Domain.Entities;
+using DecorRental.Domain.Repositories;
 using DecorRental.Domain.ValueObjects;
 
 namespace DecorRental.Application.UseCases.ReserveKit;
 
 public class ReserveKitHandler
 {
+
+    private readonly IKitRepository repository;
+
+    public ReserveKitHandler(IKitRepository repository)
+    {
+        this.repository = repository;
+    }
+
     public void Handle(Kit kit, ReserveKitCommand reserveKitCommand)
     {
+        var kit = _repository.GetById(reserveKitCommand.KitId)
+            ?? throw new Exception("Kit not found");
+
         var period = new DateRange(
             reserveKitCommand.StartDate,
             reserveKitCommand.EndDate);
 
         kit.Reserve(period);
+
+        _repository.Save(kit);
     }
 }
