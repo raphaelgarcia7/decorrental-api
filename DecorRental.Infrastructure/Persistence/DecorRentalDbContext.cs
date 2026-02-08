bc65218364933c0ceff404383a8cc848c07dfd93
raphaelgarcia7 <raphaelmelogarcia@gmail.com>
2026-02-08 11:26:05 -0300

    feat(infra): add dbcontext with kit and reservations

---
+++ DecorRental.Infrastructure/Persistence/DecorRentalDbContext.cs
@@ -0,0 +1,23 @@
+﻿using DecorRental.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DecorRental.Infrastructure.Persistence;
+
+public class DecorRentalDbContext : DbContext
+{
+    public DbSet<Kit> Kits => Set<Kit>();
+    public DbSet<Reservation> Reservations => Set<Reservation>();
+
+    public DecorRentalDbContext(DbContextOptions<DecorRentalDbContext> options)
+        : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Kit>()
+            .HasMany(kit => kit.Reservations)
+            .WithOne()
+            .HasForeignKey("KitId");
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
