eeb5af4322ff2e1adbb5f4681213bfdeb78c97f5
raphaelgarcia7 <raphaelmelogarcia@gmail.com>
2026-02-08 11:29:03 -0300

    feat(infra): add ef core kit repository

---
+++ DecorRental.Infrastructure/Repositories/EfKitRepository.cs
@@ -0,0 +1,27 @@
+﻿using DecorRental.Domain.Entities;
+using DecorRental.Domain.Repositories;
+using DecorRental.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace DecorRental.Infrastructure.Repositories;
+
+public class EfKitRepository : IKitRepository
+{
+    private readonly DecorRentalDbContext _context;
+
+    public EfKitRepository(DecorRentalDbContext context)
+    {
+        _context = context;
+    }
+
+    public Kit? GetById(Guid id)
+        => _context.Kits
+            .Include(kit => kit.Reservations)
+            .FirstOrDefault(kit => kit.Id == id);
+
+    public void Save(Kit kit)
+    {
+        _context.Update(kit);
+        _context.SaveChanges();
+    }
+}
